1b9eed1e461c4c3835ed123b01d0daef1b4a218c
MianMianSHAN <mianmian-shan@hotmail.com>
2022-10-13 23:47:03 +0200

    #ajout des boutons

--- myComponents/player/index.js
+++ myComponents/player/index.js
@@ -8,80 +8,168 @@ class myComponent extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
-    //this.src = this.getAttribute('src');
-    /*<audio id="player" src="${this.src}" controls></audio>*/
-    this.playList = [
-      {
-        url: "https://github.com/Mian4SHAN/Projet-WebComponents.git/MyComponents/sounds/0.mp3",
-        author: "Kavinsky",
-        title: "Nightcall",
-        index: 0,
-      },
-      {
-        url: "https://dorian-chapoulie.github.io/tp_webcomponents/sounds/1.mp3",
-        author: "DaftPunk",
-        title: "One More Time",
-        index: 1,
-      },
-      {
-        url: "https://dorian-chapoulie.github.io/tp_webcomponents/sounds/2.mp3",
-        author: "CyberPunk",
-        title: "Never fade away",
-        index: 2,
-      },
-      {
-        url: "https://dorian-chapoulie.github.io/tp_webcomponents/sounds/3.mp3",
-        author: "System of a down",
-        title: "Toxicity",
-        index: 3,
-      }
-    ];
-    this.currentSoundObject = this.playList[0];
-    this.createIds();
+    this.src = this.getAttribute('src');
+
+    this.playlist = ["./sounds/M1_mp3.mp3","./sounds/M2_mp3.mp3","./sounds/M3_mp3.mp3","./sounds/M4_mp3.mp3","./sounds/M5_mp3.mp3"];
+    this.titre =["Your name","Shinzou wo Sasageyo","紅蓮華","Silhouette","We are"];
+    this.artiste =["RADWIMPS","Linked Horizon","LiSA","KANA-BOON","Hiroshi Kitadani"];
+    this.picture = ["./assets/yourname.jpg","./assets/attack.jpg","./assets/demonSlayer.jpg","./assets/naruto.jpg","./assets/onePiece.jpg"];
+    this.index = 0;
+    this.src = this.playlist[this.index];
+    this.titreMusique = this.titre[this.index];
+    this.nomArtiste = this.artiste[this.index];
+    this.albumPhoto = this.picture[this.index];
   }
 
   connectedCallback() {
     // Do something
     this.shadowRoot.innerHTML = `
         <style>
-            h1 {
-                color:red;
+            img {
+              zoom: 3%;
             }
+            .music-player{
+              width: 350px;
+              height: 500px;
+              border-radius: 20px;
+              background-color: rgba(255,255,255,0.08);
+              box-shadow: 0 40px 100px rgba(255,255,255,0.1);
+              padding: 30px;
+              overflow: hidden;
+              color: #d5eebb;
+          }
+
+          .titre,
+          .artiste{
+            text-align: center;
+            text-transform: capitalize;
+          }
+          
+          .titre{
+            font-zise: 40px;
+            font-weight: 500;
+            margin-bottom: 10px;
+          }
+
+          .artiste{
+            font-zise: 20px;
+            border-bottom: 1px solid #d5eebb;
+          }   
+
+          .disque{
+            position: relative;
+            display: block;
+            margin: 40px auto;
+            width: 180px;
+            height: 180px;
+            border-radius: 50%;
+            background-image: url("myComponents/assets/totoro.jpg");
+            background-size: cover;
+            box-shadow: 0 0 0 10px rgba(255, 255, 255, 0.08);
+            animation: rotate 20s linear infinite;
+          }
+
+          .slider{
+            width: 100%;
+            position: relative;
+          }
+
+          .chargement{
+            -webkit-appearance: none;
+            width: 100%;
+            height: 5px;
+            border-radius: 10px;
+            background-color: #6d55aa;
+            overflow: hidden;
+            cursor: pointer;
+          }
+          
+          .chargement::-webkit-slider-thumb{
+            -webkit-appearance: none;
+            width: 10px;
+            height: 20px;
+            background-color: black;
+            box-shadow: -400px 0 0 400px #4b28d7;
+          }
+
+          .debut,.fin{
+            font-size: 14px;
+          }
+          .fin{
+            position: absolute;
+            right: 0;
+          }
+
+          @keyframes rotate{
+            0%{
+              transform: rotate(0);
+            }
+            100%{
+              transform: rotate(360deg);
+            }
+          }
+
+          .menu{
+            position: relative;
+            display: block;
+            text-align: center;
+          }
+
         </style>
-        <h1>lecteur audio amélioré</h1>
-        <audio id="player" src="${this.src}" controls></audio>
+        
+        <audio id="player" src="${this.src}"></audio>
+
+        <div class="music-player">
+          <h1 class="titre">song : ${this.titreMusique}</h1>
+          <p class="artiste">by artist : ${this.nomArtiste}</p>
+          <div class="disque"></div>
+
+          <br>
+          <div class="slider">
+              <input type="range" value="0" class="chargement">
+              <br>
+              <span class="debut">00:00</span> 
+              <span class="fin">00:00</span> 
+          </div>
+          
+          
+          <br>
+          <div class="menu">
+          <button id="play"><img src="myComponents/assets/circle-play.png"></button>
+          <button id="pause"><img src="myComponents/assets/circle-pause.png"></button>
+          <button id="stop"><img src="myComponents/assets/circle-stop.png"></button>
+          <button id="zero"><img src="myComponents/assets/reply.png"></button>
+          <button id="moins"><img src="myComponents/assets/backward-fast.png"></button>
+          <button id="plus"><img src="myComponents/assets/forward-fast.png"></button>
+          <button id="pre"><img src="myComponents/assets/backward.png"></button>
+          <button id="next"><img src="myComponents/assets/forward.png"></button>
+          </div>
+          <br>
+          <br>
+          <label>Volume 
+            <input id="volumeSlider" 
+            type="range" min=0 max=2 step=0.1 value="1">
+          </label>
+        </div>
         <br>
-        <br>
-        <button id="play">Play</button>
-        <button id="pause">Pause</button>
-        <button id="stop">Stop</button>
-        <button id="zero">Retour à 0</button>
-        <br>
-        <br>
-        <button id="plus">+10s</button>
-        <button id="moins">-10s</button>
-        <br>
-        <label>Volume 
-          <input id="volumeSlider" 
-          type="range" min=0 max=2 step=0.1 value="1">
-        </label>
-        <br>
-        <webaudio-knob 
-          id="volumeKnob" 
-          src="./assets/knobs/vernier.png" 
-          value="1" max="2" step="0.1" diameter="128" sprites="50" 
-          valuetip="0" tooltip="Volume">
-        </webaudio-knob>
-    `;
+    `
+    ;
 
     this.fixRelativeURLs();
 
     this.player = this.shadowRoot.querySelector('#player');
-    this.volumeKnob = this.shadowRoot.querySelector('#volumeKnob');
     this.volumeSlider = this.shadowRoot.querySelector('#volumeSlider');
+    this.chargement = this.shadowRoot.querySelector('.chargement');
+    this.titre = this.shadowRoot.querySelector('.titre');
+    this.artiste = this.shadowRoot.querySelector('.artiste');
+    this.disque = this.shadowRoot.querySelector('.disque');
+    this.depart = this.shadowRoot.querySelector('.debut');
+    this.fin =  this.shadowRoot.querySelector('.fin');
+
+
     this.defineListeners();
   }
-
+  
   fixRelativeURLs() {
     const baseURL = getBaseURL();
     console.log('baseURL', baseURL);
@@ -98,6 +186,7 @@ class myComponent extends HTMLElement {
     }
   }
 
+  
   defineListeners() {
     this.shadowRoot.querySelector('#play').addEventListener('click', () => {
       this.player.play();
@@ -105,6 +194,7 @@ class myComponent extends HTMLElement {
 
     this.shadowRoot.querySelector('#pause').addEventListener('click', () => {
       this.player.pause();
+      this.disque.pause();
     });
     this.shadowRoot.querySelector('#stop').addEventListener('click', () => {
       this.player.pause();
@@ -119,16 +209,33 @@ class myComponent extends HTMLElement {
     this.shadowRoot.querySelector('#moins').addEventListener('click', (evt) => {
       this.player.currentTime = this.player.currentTime - 10;
     });
+
+    //
+    this.shadowRoot.querySelector('#pre').addEventListener('click', (evt) => {
+      this.index--;
+      if (this.index < 0) {
+        this.index = this.playlist.length - 1;
+      }
+      this.player.src = this.playlist[this.index];
+      this.src = this.playlist[this.index];
+      this.player.play()
+    });
+
+    //passer à la musique suivante
+    this.shadowRoot.querySelector('#next').addEventListener('click', (evt) => {
+      this.index++
+      if (this.index > this.playlist.length - 1) {
+        this.index = 0;
+      }
+      this.player.src = this.playlist[this.index];
+      this.src = this.playlist[this.index];
+      this.player.play()
+    });
     //input = value 
     // le player prend le volume 
     this.shadowRoot.querySelector('#volumeSlider').addEventListener('input', (evt) => {
       this.player.volume = evt.target.value;
-      this.volumeKnob.value = evt.target.value;
-    });
-    this.shadowRoot.querySelector('#volumeKnob').addEventListener('input', (evt) => {
-      this.player.volume = evt.target.value;
-      this.volumeSlider.value = evt.target.value;
-    });
+    });  
     
   }
 }
